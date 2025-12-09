662118076caa1b7b2e8bf1753d96756408325ee7
Henrik Ygge <henrik@meepo.se>
2025-12-09 20:12:15 +0100

    solved day 9

--- src/main/java/Day9.java
+++ src/main/java/Day9.java
@@ -1,32 +1,47 @@
 import util.Pos;
 import util.Util;
 
-import java.util.Arrays;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day9 {
 
     public static void main(String[] args) {
-        Util.verifySubmission();
-        //var input = Util.readIntLists();
-        var input = Arrays.asList(
-                Arrays.asList(7, 1),
-                Arrays.asList(11, 1),
-                Arrays.asList(11, 7),
-                Arrays.asList(9, 7),
-                Arrays.asList(9, 5),
-                Arrays.asList(2, 5),
-                Arrays.asList(2, 3),
-                Arrays.asList(7, 3));
-        //Util.submitPart1(solve1(input));
+        var input = Util.readIntLists();
+        Util.submitPart1(solve1(input));
         Util.submitPart2(solve2(input));
     }
 
     private static long solve2(List<List<Integer>> input) {
-        var posList = input.stream()
-                .map(a -> new Pos(a.getFirst(), a.get(1)))
-                .toList();
-
+        int[] xList = new int[input.size()];
+        int[] yList = new int[input.size()];
+        var posList = new ArrayList<Pos>();
+        for (int i = 0; i < input.size(); i++) {
+            List<Integer> row = input.get(i);
+            xList[i] = row.get(0);
+            yList[i] = row.get(1);
+            posList.add(new Pos(row.get(0), row.get(1)));
+        }
+        var polygon = new Polygon(xList, yList, xList.length);
+        long max = 0;
+        for (int i = 0; i < posList.size(); i++) {
+            var posA = posList.get(i);
+            for (int j = i + 1; j < posList.size(); j++) {
+                var posB = posList.get(j);
+                int minX = Math.min(posA.x, posB.x);
+                int minY = Math.min(posA.y, posB.y);
+                int maxX = Math.max(posA.x, posB.x);
+                int maxY = Math.max(posA.y, posB.y);
+                if (polygon.contains(new Rectangle(minX + 1, minY + 1, maxX - minX - 1, maxY - minY -1 ))) {
+                    long area = (long) (Math.abs(posA.x - posB.x) + 1) * (Math.abs(posA.y - posB.y) + 1);
+                    if (area > max) {
+                        max = area;
+                    }
+                }
+            }
+        }
+        return max;
     }
 
     private static long solve1(List<List<Integer>> input) {
